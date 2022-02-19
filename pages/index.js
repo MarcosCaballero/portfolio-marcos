0063976578af5a9d8250f4375b69fcc4cb78bee1
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-02-19 02:18:58 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import React from "react";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar/Navbar";
 
 export default function Home() {
