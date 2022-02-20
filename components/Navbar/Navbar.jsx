cb4e5481534adf7a292880997fe36b2cedbcf345
Marcos Caballero <77732998+MarcosCaballero@users.noreply.github.com>
2022-02-20 00:02:16 -0300

    location functionality UI

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import Link from "next/link";
 import { routes } from "./routesOps";
+import Banner from "../Banner/Banner";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+  console.log(router.pathname);
   return (
     <header className="w-full fixed -top-0 bg-white flex justify-between items-center">
-      <div className="xlm:w-full mx-auto p-5 flex justify-center "></div>
-      <nav className="flex w-full items-baseline justify-evenly">
+      <Banner text="marcos caballero full stack developer" />
+      <nav className="flex w-2/4 items-baseline justify-evenly">
         {routes.map((route, index) => (
           <Link key={index} href={route.ph}>
-            <a className="text-xl uppercase">{route.name}</a>
+            <a
+              className={
+                router.pathname === route.ph
+                  ? "text-xl uppercase scale-125"
+                  : "text-xl uppercase "
+              }
+            >
+              {route.name}
+            </a>
           </Link>
         ))}
       </nav>
